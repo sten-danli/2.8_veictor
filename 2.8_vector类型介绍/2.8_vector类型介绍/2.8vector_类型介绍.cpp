d235dce407627799eb823d592f87f0fc599c77b9
Papa ist Blod <akiraryo@hotmail.com>
2019-08-04 00:19:51 +0200

    //vector容器常用操作与内存释放

--- 2.8_vector类型介绍/2.8_vector类型介绍/2.8vector_类型介绍.cpp
+++ 2.8_vector类型介绍/2.8_vector类型介绍/2.8vector_类型介绍.cpp
@@ -3,27 +3,35 @@
 #include<vector>
 using namespace std;
 
+//vector容器常用操作与内存释放
+//实践程序:
+//ServerName = 1区  //表示服务器名称
+//ServerID = 100000 //表示服务器ID
+
+struct conf
+{
+	char itemname[40];
+	char itemcontent[100];
+};
+
 int main()
 {
-//三:vector 迭代器失败举例说明
-vector<int> vector_value_1 = {6,5,4,3,2,1};
-auto beg = vector_value_1.begin();
-auto end = vector_value_1.end();
-for (auto x : vector_value_1)
-{
-	vector_value_1.insert(end,888);//因为迭代器无法从新定位begin和end所以无法这样加入内容。
+	conf* pconf1 = new conf;
+	strcpy_s(pconf1->itemname, sizeof(pconf1->itemname), "ServerName");
+	strcpy_s(pconf1->itemcontent, sizeof(pconf1->itemcontent), "Zone 1");
+
+	conf* pconf2 = new conf;
+	strcpy_s(pconf2->itemname, sizeof(pconf2->itemname), "ServerName");
+	strcpy_s(pconf2->itemcontent, sizeof(pconf2->itemcontent), "100000");
+
+	vector<conf*> conflist;
+	conflist.push_back(pconf1);
+	conflist.push_back(pconf2);
+
 	
-	break;					//除非在此处break。
+	
+	
 }
-for (auto beg = vector_value_1.begin(),end=vector_value_1.end(); beg!=end; beg++ )//然后从新创迭代器打印。
-{
-	cout << *beg;
-	cout << "-";
-}
-
-
-
-
 //二:vector 和 struct 一起使用方法举例实现
 //struct familien
 //{
@@ -42,6 +50,7 @@ for (auto beg = vector_value_1.begin(),end=vector_value_1.end(); beg!=end; beg++
 //
 //	vector<familien>::iterator iter;
 //	iter = familienChina.begin();
+//	
 //	while (iter != familienChina.end())
 //	{
 //		cout << iter->mama << endl;
@@ -49,8 +58,83 @@ for (auto beg = vector_value_1.begin(),end=vector_value_1.end(); beg!=end; beg++
 //		cout << (*iter).honora << endl;
 //		iter++;
 //	}
+//}
+
+//代送器便利string类型举例。
+//int main()
+//{
+//	string str = { "in der nach bin ich aleine und gluecklich." };
+//	auto begin = str.begin();
+//	auto end = str.end();
+//	
+//	for (auto itr = begin; itr!=end; ++itr)
+//	{
+//		*itr = toupper(*itr);
+//		cout << *itr;
+//	}
+
+	/*for (auto &x : str)
+	{
+		x = toupper(x);
+		cout << x;
+	}*/
+	
+//}
+
+//int main()
+//{
+	//int icount = 0;
+	//vector<int> vector_value_1 = { 1,2,3,4,5};
+	//vector<int> vector_value_2 = {6,7,8};
+	//auto beg_value2 = vector_value_2.begin();
+	//auto end_value2 = vector_value_2.end();
+
+	//auto beg = vector_value_1.begin();
+	//auto end = vector_value_1.end();
+
+	//vector_value_1.insert(end, beg_value2, end_value2);
+	///*while (beg != vector_value_1.end())
+	//{
+	//	beg = vector_value_1.insert(beg, icount + 80);
+	//	icount++;
+	//	if (icount > 10)
+	//	{
+	//		break;
+	//	}
+	//	beg++;
+	//}*/
+	//
+	// beg = vector_value_1.begin();
+	// end = vector_value_1.end();
+	//while (beg != end)
+	//{
+	//	cout << *beg << "->";
+	//	beg++;
+	//}
+
+//三:vector 迭代器失败举例说明
+//int main()
+//{
+//	vector<int> vector_value_1 = {6,5,4,3,2,1};
+//	auto beg = vector_value_1.begin();
+//	auto end = vector_value_1.end();
+//	for (auto x : vector_value_1)
+//	{
+//		vector_value_1.insert(end,888);//因为迭代器无法从新定位begin和end所以无法这样加入内容。
+//		
+//		break;					//除非在此处break。
+//	}
+//	for (auto beg = vector_value_1.begin(),end=vector_value_1.end(); beg!=end; beg++ )//然后从新创迭代器打印。
+//	{
+//		cout << *beg;
+//		cout << "-";
+//	}
+//}
+
 
 	//一:初认识vector容器
+//int main()
+//{
 	//vector<int> iv = { 22,33,44,55,66 };//定义和初始化vector对象。
 	//vector<int> ::iterator iter_first;
 	//iter_first= iv.begin();	//定义代送器begin()。
@@ -70,5 +154,4 @@ for (auto beg = vector_value_1.begin(),end=vector_value_1.end(); beg!=end; beg++
 	//}
 	//iter_first = iv.end();//定义代送器end()
 	//cout << *(iv.end()-1);//end()不是指向最后一个数字，而是最后值的后一位，所以要想取最后一位值得要-1。
-
-}
+//}
